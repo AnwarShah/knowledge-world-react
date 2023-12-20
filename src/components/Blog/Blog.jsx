cf361025764b0f932a7dbd44c2c3d4255bf971ca
Mohammad Anwar Shah <mohammadanwarshah@gmail.com>
2023-12-20 15:41:38 +0600

    finished 42-4

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,23 +1,29 @@
 import PropTypes from 'prop-types';
 
 const Blog = ({blog}) => {
-    const {title, cover, author, author_img, reading_time} = blog;
+    const {title, cover, author, author_img, posted_date, reading_time, hashtags} = blog;
 
     return (
         <div>
             <img src={cover} alt={`Cover picture of the blog ${title}`} />
             <div className='flex justify-between'>
-                <div>
-                    <img src={author_img} alt="" />
+                <div className='flex gap-6'>
+                    <img className='w-16' src={author_img} alt="" />
                     <div>
-
+                        <h3 className="text-2xl">{author}</h3>
+                        <p className="text-xl">{posted_date}</p>
                     </div>
                 </div>
                 <div>
-                    <span>{reading_time}</span>
+                    <span>{reading_time} min read</span>
                 </div>
             </div>
-            <h2 className='text-4xl'>{title}</h2>        
+            <h2 className='text-4xl'>{title}</h2>
+            <p>
+                {
+                    hashtags.map( (hash,idx) => <span key={idx}><a href="#">#{hash}</a> </span>)
+                }
+            </p>      
         </div>
     );
 };
